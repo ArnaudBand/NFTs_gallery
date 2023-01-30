630c143bfa1ae0e19ab39bd52100549865745ac8
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-01-30 10:33:12 +0300

    Fetch nfts collections

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,6 +7,7 @@ const Home = () => {
   const [wallet, setWallet] = useState("");
   const [collection, setCollection] = useState("");
   const [NFTs, setNFTs] = useState([]);
+  const [fetchForCollection, setFetchForCollection] = useState(false);
 
   const fetchNfts = async() => {
     let nfts; 
@@ -35,15 +36,34 @@ const Home = () => {
     }
   }
 
+  const fetchNFTsForCollection = async() => {
+    let nfts;
+    var requestOptions = {
+      method: 'GET'
+    };
+    if(collection.length) {
+      const api_key = "dQUbHnCVVa5K0Sg0kEqcOzqgbxmlcQhR";
+      const baseURL = `https://eth-mainnet.g.alchemy.com/v2/${api_key}/getNFTsForCollection/`;
+      const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}`;
+      nfts = await fetch(fetchURL, requestOptions).then((data) => data.json())
+    }
+    if(nfts) {
+      console.log("NFTS in collection: ", nfts);
+    }
+  }
+
   return (
     <div className="flex flex-col items-center justify-center py-8 gap-y-3">
     <div className="flex flex-col w-full justify-center items-center gap-y-2">
       <input onChange={(e) => { setWallet(e.target.value) }} value={wallet} type={"text"} placeholder="Add your wallet address" />
       <input onChange={(e) => { setCollection(e.target.value) }} value={collection} type={"text"} placeholder="Add the collection address" />
-      <label className="text-gray-600 "><input type={"checkbox"} className="mr-2"></input>Fetch for collection</label>
+      <label className="text-gray-600 ">
+        <input onChange={(e) => { setFetchForCollection(e.target.checked) }} type={"checkbox"} className="mr-2"/>Fetch for collection</label>
       <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"} onClick={
         () => {
-          fetchNfts()
+          if (fetchForCollection) {
+            fetchNFTsForCollection();
+          } else fetchNfts();
         }
       }>Let's go! </button>
     </div>
