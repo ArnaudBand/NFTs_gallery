164d82a0f132aace98bb2bcb3767ed6efd17cac8
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-01-30 21:13:22 +0300

    Import nftCard

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { useState } from 'react'
+import { useState } from 'react';
+import NftCard from './components/NftCard';
 
 const Home = () => {
 
@@ -38,10 +39,10 @@ const Home = () => {
 
   const fetchNFTsForCollection = async() => {
     let nfts;
-    var requestOptions = {
-      method: 'GET'
-    };
     if(collection.length) {
+      var requestOptions = {
+        method: 'GET'
+      };
       const api_key = "dQUbHnCVVa5K0Sg0kEqcOzqgbxmlcQhR";
       const baseURL = `https://eth-mainnet.g.alchemy.com/v2/${api_key}/getNFTsForCollection/`;
       const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}`;
@@ -49,24 +50,35 @@ const Home = () => {
     }
     if(nfts) {
       console.log("NFTS in collection: ", nfts);
+      setNFTs(nfts.nfts);
     }
   }
 
   return (
     <div className="flex flex-col items-center justify-center py-8 gap-y-3">
-    <div className="flex flex-col w-full justify-center items-center gap-y-2">
-      <input onChange={(e) => { setWallet(e.target.value) }} value={wallet} type={"text"} placeholder="Add your wallet address" />
-      <input onChange={(e) => { setCollection(e.target.value) }} value={collection} type={"text"} placeholder="Add the collection address" />
-      <label className="text-gray-600 ">
-        <input onChange={(e) => { setFetchForCollection(e.target.checked) }} type={"checkbox"} className="mr-2"/>Fetch for collection</label>
-      <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"} onClick={
-        () => {
-          if (fetchForCollection) {
-            fetchNFTsForCollection();
-          } else fetchNfts();
+      <div className="flex flex-col w-full justify-center items-center gap-y-2">
+        <input disabled={fetchForCollection} className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50" onChange={(e) => { setWallet(e.target.value) }} value={wallet} type={"text"} placeholder="Add your wallet address" />
+        <input className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50" onChange={(e) => { setCollection(e.target.value) }} value={collection} type={"text"} placeholder="Add the collection address" />
+        <label className="text-gray-600 ">
+          <input onChange={(e) => { setFetchForCollection(e.target.checked) }} type={"checkbox"} className="mr-2"/>Fetch for collection</label>
+        <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"} onClick={
+          () => {
+            if (fetchForCollection) {
+              fetchNFTsForCollection();
+            } else fetchNfts();
+          }
+        }>Let's go! </button>
+      </div>
+      <div className='flex flex-row flex-wrap gap-y-12 mt-4 w-5/6 gap-x-2 justify-center'>
+        {
+          NFTs.length && NFTs.map((nft, index) => (
+            <div key={index }>
+              <NftCard nft={nft} />
+            </div>
+
+          ))
         }
-      }>Let's go! </button>
-    </div>
+      </div>
   </div>
   )
 }
